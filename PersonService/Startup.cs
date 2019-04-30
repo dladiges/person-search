4bc453aace7ed89857f59197cd0f31c12cba4614
dladiges <github@ladiges.net>
2019-04-30 15:03:09 -0700

    Adjust CORS policy for allowing localhost calls.

--- PersonService/Startup.cs
+++ PersonService/Startup.cs
@@ -54,7 +54,7 @@ namespace PersonService
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseCors();
+            app.UseCors("LocalhostPolicy");
             app.UseMvc();
         }
     }
