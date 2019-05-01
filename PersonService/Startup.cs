0206ebd2228ee10b013be5bf41aad118784faed6
dladiges <github@ladiges.net>
2019-05-01 08:36:43 -0700

    Ignore reference loop Person->Inerest->Person in json serialization. Remove unused usings.

--- PersonService/Startup.cs
+++ PersonService/Startup.cs
@@ -44,6 +44,8 @@ namespace PersonService
             services.AddScoped<ISearchService, SearchService>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.Configure<MvcJsonOptions>(config => config.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
